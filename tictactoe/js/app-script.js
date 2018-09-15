7a95779872cbf16840d770b924046d2b44b29b8a
Vladislav Reznikov <vladrezdev@gmail.com>
2018-09-14 23:44:43 -0700

    working wining objective implementation

--- tictactoe/js/app-script.js
+++ tictactoe/js/app-script.js
@@ -1,7 +1,13 @@
 //Global variable for current player name
 var currentPlayerName = 'X';
-//jquery: set text for span name
-$('span[name="whoseturn"]').text('X');
+var rowName;
+var columnName;
+
+var gameOver = false;
+
+$('span[name="whoseturn"]').text(currentPlayerName);
+$('.gameover').hide();
+
 //apply highlight/unhighlight css to class player
 $('.player[name="X"]').addClass('highlight');
 $('.player[name="O"]').addClass('unhighlight');
@@ -10,20 +16,28 @@ $('td').click(
     function() {
         if(currentPlayerName == 'X'){
             $(this).text('X');
-            //set text from X to O
-            $('span[name="whoseturn"]').text('O');
+        } else {
+            $(this).text('O');
+        }
+        rowName = $(this).parent().attr("name");
+        columnName = $(this).attr("name");
+        if(checkIfCurrentPlayerWon_CheckRow() || checkIfCurrentPlayerWon_CheckColumn()){
+            gameOver = true;
+            //
+            $('.gameover').show();
+            $('span[name="whowon"]').text(currentPlayerName);
+        }
+        if (currentPlayerName == 'X') {
             //Switch button highlighting
             $('.player[name="X"]').removeClass('highlight');
             $('.player[name="X"]').addClass('unhighlight');
 
             $('.player[name="O"]').removeClass('unhighlight');
             $('.player[name="O"]').addClass('highlight');
-
             currentPlayerName = 'O';
-        } else {
-            $(this).text('O');
             //set text from X to O
-            $('span[name="whoseturn"]').text('X');
+            $('span[name="whoseturn"]').text('O');
+        } else {
             //Switch button highlighting
             $('.player[name="O"]').removeClass('highlight');
             $('.player[name="O"]').addClass('unhighlight');
@@ -32,6 +46,30 @@ $('td').click(
             $('.player[name="X"]').addClass('highlight');
 
             currentPlayerName = 'X';
+            //set text from X to O
+            $('span[name="whoseturn"]').text('X');
         }
     }
 );
+
+function checkIfCurrentPlayerWon_CheckRow() {
+    var currentRow = $('tr[name="' + rowName +'"');
+    var currentPlayerWon = true;
+    currentRow.children().each(function(){
+        if($(this).text() != currentPlayerName){
+            currentPlayerWon = false;
+        }
+    });
+    return currentPlayerWon;
+}
+
+function checkIfCurrentPlayerWon_CheckColumn(){
+    var currentPlayerWon = true;
+    $('tr').each(function(){
+       var col = $(this).find('td[name=' + columnName +']');
+       if(col.text() != currentPlayerName){
+           currentPlayerWon = false;
+       }
+    });
+    return currentPlayerWon;
+}
